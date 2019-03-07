53e5d5eb59dd1ff5eb386206232b56736be4a0f2
zhangbokang <540064991@qq.com>
2019-03-07 12:17:47 +0800

    创建项目

---
+++ src/main/java/com/mycharx/qdf/QdfApplication.java
@@ -0,0 +1,13 @@
+package com.mycharx.qdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QdfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QdfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mycharx/qdf/QdfApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mycharx.qdf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QdfApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
